f86fdb3b88035f812fa263d70362356ca39fc172
yurim222 <yurim@ajou.ac.kr>
2023-04-15 00:34:57 +0900

    changed_apikey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key="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"
+openai.api_key="Yourapi"
 
 completion=openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
